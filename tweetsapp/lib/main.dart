95dad8b38357a4c29f3e1a95bc92c838411f1822
anushakn <anusha01061999@gmail.com>
2025-01-27 11:21:11 +0530

    integrated firebase

--- tweetsapp/lib/main.dart
+++ tweetsapp/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tweetsapp/design/app_theme.dart';
 import 'package:tweetsapp/features/onboarding/ui/onboarding_acreen.dart';
+import 'package:tweetsapp/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
