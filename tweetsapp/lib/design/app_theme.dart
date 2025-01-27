167097cf6f8af17b84cf610f04b5d1d9b613b0c3
61077238 <himayu.x.kokku@kp.org>
2025-01-27 11:35:00 +0530

    register screen ui

--- tweetsapp/lib/design/app_theme.dart
+++ tweetsapp/lib/design/app_theme.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      appBarTheme:
+          AppBarTheme(backgroundColor: Colors.grey.shade900, elevation: 0));
 }
